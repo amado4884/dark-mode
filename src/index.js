738e7eb30e111775a042aa9075f6b452388e1873
Amado Guerrero <amado4884@gmail.com>
2020-08-13 04:58:34 -0400

    removed log

--- src/index.js
+++ src/index.js
@@ -13,10 +13,6 @@ const App = () => {
   const [darkMode, setDarkMode] = useDarkMode(false);
 
   useEffect(() => {
-    console.log("Dark Mode changed");
-  }, [darkMode]);
-
-  useEffect(() => {
     axios
       .get(
         "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=true"
